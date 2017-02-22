98266443c1adfaba155956425a8e3ad5dd808ed5
IC <ivan.ceban@gmail.com>
2017-02-22 13:37:59 +0200

    homepage search block tab added

--- index.php
+++ index.php
@@ -155,9 +155,99 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="tab-pane fade in sale_block">
-                            <div class="row">
-                                <div class="col-xs-12">Sale</div>
+                        <div class="tab-pane fade in sale_block search_block">
+                            <div class="row margin_top_10">
+                                <div class="col-xs-12 col-sm-4 col-md-6 col-lg-4 margin_top_15">
+                                    <label class="form_el_label dark"><i class="icn icon-building"></i><span>Type de bien</span></label>
+                                    <select  multiple="multiple" name="object_type" data-actions-box="true">
+                                        <option value="House">House</option>
+                                        <option value="Apartment">Apartment</option>
+                                        <option value="Building plot blabla text">Building plot blabla text</option>
+                                        <option value="Building">Building</option>
+                                        <option value="Parking space">Parking space</option>
+                                    </select>
+                                </div>
+                                <div class="col-xs-12 col-sm-4 col-md-6 col-lg-4 margin_top_15">
+                                    <label class="form_el_label dark"><i class="icn icon-address"></i><span>Lieu</span></label>
+                                    <select multiple="multiple" name="object_place">
+                                        <option value="Geneva">Geneva</option>
+                                        <option value="Bern">Bern</option>
+                                        <option value="Zurich">Zurich</option>
+                                        <option value="Paris">Paris</option>
+                                    </select>
+                                </div>
+                                <div class="col-xs-12 col-sm-4 col-md-12 col-lg-4 margin_top_15">
+                                    <label class="form_el_label dark"><i class="icn icon-bedroom"></i><span>Projects Neufs</span></label>
+                                    <select multiple="multiple" name="object_subtype">
+                                        <option value="Bastide">Bastide</option>
+                                        <option value="Bungalow">Bungalow</option>
+                                        <option value="Chalet">Chalet</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row margin_top_10">
+                                <div class="col-xs-12 col-sm-4 col-md-6 col-lg-4 margin_top_15">
+                                    <label class="form_el_label dark"><i class="icn icon-rooms"></i><span>Pièces</span></label>
+                                    <div class="min_select_container">
+                                        <select multiple="multiple" name="object_rooms_number">
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-4 col-md-6 col-lg-4 margin_top_15">
+                                    <label class="form_el_label dark"><i class="icn icon-bedroom"></i><span>Chambres</span></label>
+                                    <div class="min_select_container">
+                                        <select multiple="multiple" name="object_price">
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5+</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-4 col-md-12 col-lg-4 margin_top_15">
+                                    <label class="form_el_label dark"><i class="icn icon-living_area"></i><span>Surface habit</span></label>
+                                    <div class="min_select_container">
+                                        <select multiple="multiple" name="object_surface">
+                                            <option value="100">100</option>
+                                            <option value="200">200</option>
+                                            <option value="300">300</option>
+                                            <option value="400">400</option>
+                                            <option value="500+">500+</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="row margin_top_10">
+                                <div class="col-xs-12 col-sm-6 margin_top_15">
+                                    <label class="form_el_label dark"><i class="icn icon-price"></i><span>Prix min</span></label>
+                                    <div class="input_container dark">
+                                        <input type="text" placeholder="Min">
+                                        <div class="input_label">CHF</div>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-6 margin_top_15">
+                                    <label class="form_el_label dark"><i class="icn icon-price"></i><span>Prix max</span></label>
+                                    <div class="input_container dark">
+                                        <input type="text" placeholder="Max">
+                                        <div class="input_label">CHF</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row margin_top_15">
+                                <div class="col-xs-12 margin_top_15">
+                                    <div class="input_container dark">
+                                        <input type="text" placeholder="Rechercher votre bien">
+                                        <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
