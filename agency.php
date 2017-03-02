3d8e77364e4a2fad7025c4a889b86d6561606a8e
IC <ivan.ceban@gmail.com>
2017-03-02 16:58:56 +0200

    agency page added

---
+++ agency.php
@@ -0,0 +1,119 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Rousseau 5 - L'agance</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/libraries/bootstrap.css">
+    <link rel="stylesheet" href="css/libraries/bootstrap-multiselect.css">
+    <link rel="stylesheet" href="css/libraries/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/libraries/tooltipster.bundle.min.css">
+    <link rel="stylesheet" href="css/custom_icons/style.css">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/media_queries.css">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="agency">
+
+<div class="item_active_background"></div>
+
+<?php $current_page = 'agency'?>
+
+<?php include 'includes/header.php'?>
+<?php include 'includes/main_nav.php'?>
+
+<main>
+    <section class="agency_img_section">
+        <div class="container-fluid">
+            <div class="agency_img_container">
+                <img src="img/agency_img.jpg" alt="">
+                <div class="gradient_bottom"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="agency_info_section">
+        <div class="container-fluid">
+            <div class="agency_info_container">
+                <h1>L'agence</h1>
+                <p>Notre travail consiste à rechercher sur le territoire Suisse des appartements, villas et propriétés. Qu’ils soient meublés ou vides, nous les évaluons et les répertorions afin de vous les proposer à la location ou pour les acquérir directement.</p>
+                <p>Location d’appartements, de logements, de studios et de bureaux sur Suisse.  Rousseau N°5 l’agence immobilière répertorie et met à votre disposition toutes les dernières offres de location d’appartements, de logements sur notre site www.rousseau5.ch.</p>
+                <p>Toutes nos offres sont régulièrement actualisées en fonction de la disponibilité de nos dernières locations ainsi que selon nos nouvelles propositions.</p>
+                <div class="row">
+                    <div class="col-xs-12 col-md-6">
+                        <h2>ENVIE DE DEVENIR PROPRITAIRE ?</h2>
+                        <p>Découvrez toutes nos offres régulièrement mises à jour sur notre site internet principal où nous vous proposons tous les biens immobiliers disponibles à ce jour.</p>
+                        <p>Ce site est actualisé régulièrement en fonction des nouvelles offres disponibles en Suisse.</p>
+
+                        <h2>AVEZ-VOUS UN BIEN IMMOBILIER A LOUER ?</h2>
+                        <p>N’hésitez pas à nous en informer ! Nous recherchons en permanence tout type de bien immobilier situé en Suisse.</p>
+                        <p>Nos principaux appartements en location sont situés entre autre sur Suisse, parmi quelques quartiers de prédilection à Genève :</p>
+                        <p>Cologny, Vandoeuvres, Collonge-Bellerive, Florissant, Malagnou, Champel, Petit-Saconnex, Vésenaz, Eaux-vives, Plainpalais</p>
+                        <p>Sur la région Vaudoise, notre rayonnement touche les villes telles que Mies, Nyon, Rolle, Morges, Saint-Prex, Lausanne.</p>
+                        <p>ROUSSEAU N°5 L’Agence immobilière est à votre service pour vous dénicher l’appartement, la maison ou la ville recherchée.</p>
+                        <p>Vous pouvez nous mandater pour étudier et approfondir le choix de l’appartement, la maison, ou la villa qui vous correspondra le mieux en location : villa design, maison ancienne ou contemporaine.</p>
+                        <p>ROUSSEAU N°5 met tout en œuvre pour vous loger ou vous reloger via notre service de relocation. Il devient de plus en plus difficile de se loger à Genève.</p>
+                        <p>De part notre large éventail de choix de logement, toutes nos locations pourront vous permettre de choisir parmi des logements en plein centre-ville ou plutôt dans un cadre calme, tranquille et verdoyant.</p>
+                        <p>Les appartement et studios peuvent être loués meublés ou non meublés. Nous avons également des offres de bureaux en location.</p>
+
+                        <h2>TOUS LES CRITERES DE RECHERCHE PEUVENT ETRE DIVERS ET VARIES :</h2>
+                        <ul>
+                            <li>Quartier résidentiel, centre-ville, proximité écoles, transports publics proches.</li>
+                            <li>Zone piétonne, centres commerciaux, proche de la gare, desservi par les tramways.</li>
+                            <li>Proximité des principaux axes de communication, parking sous-terrain.</li>
+                        </ul>
+                        <p>Si vous ne trouvez pas de logement correspondant à votre recherche, vous pouvez nous contacter afin que l’on étudie ensemble le type de location souhaité.</p>
+                    </div>
+
+                    <div class="col-xs-12 col-md-6">
+                        <h2>LOCATION MAISON, VILLA, PAVILLON, RESIDENCE, BIEN A LOUER SUR SUISSE</h2>
+                        <p>Location de maison, de villa de pavillon et de résidence principale ou secondaire sur Cologny, Vandoeuvres, Florissant, Malagnou, Genève, Champel, Vésenaz, canton de Vaud et bien d’autres encore selon nos disponibilités sur Suisse et France.</p>
+                        <p>Sur notre site www.rousseau5.ch vous pourrez retrouver toutes nos locations de maisons, de villa, de résidences disponibles à ce jour.</p>
+                        <p>De la villa plain-pied à la maison à étage avec ou sans piscine, ROUSSEAU N°5 vous accompagne et vous assiste avant notre arrivée sur Genève : ouverture du dossier, recherche de la villa, maison, résidence selon vos critères : situation, prix, nombre de pièces, garantie bancaire, etc.</p>
+
+                        <h2>ROUSSEAU N°5 VOUS ASSISTE ET VOUS CONSEILLE DES VOTRE ARRIVEE EN SUISSE :</h2>
+                        <ul>
+                            <li>Aide à l’installation, états des lieux, abonnements internet, téléphone, électricité, assurances…</li>
+                        </ul>
+                        <p>Des villas de qualité et grand standing vous sont proposées avec terrasse, piscine, jacuzzi, véranda, pergola, jardin d’hiver, piscine extérieure ou/et intérieure (pool house)…</p>
+                        <p>Ces résidences peuvent être meublées ou non meublées, fabriquées de différents matériaux, disposées d’un abri anti atomique ou non, être dans un environnement proche de la ville et de toutes ses commodités (milieu urbain) ou plutôt situées en campagne (milieu rural).</p>
+
+                        <h2>Il est plus simple pour nous, de vous trouver la location à partir de vos critères de recherches :</h2>
+                        <ul>
+                            <li>Nombre de chambres, nombre de pièces, garage, cuisine équipée, salon, sous-sol, cave…</li>
+                        </ul>
+
+                        <h2>Les choix peuvent être extrêmement précis et variés :</h2>
+                        <ul>
+                            <li>Sans vis-à-vis, vues diverses (vue jet d’eau, vue lac de Genève, vue montagne, vue aérienne), environnement (exposition sud/ensoleillé), lumineux, calme, tranquille.</li>
+                        </ul>
+                        <p>Our job is to seek for apartment, villas, and properties in the Swiss territory.</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </section>
+
+</main>
+
+<?php include 'includes/footer.php'?>
+
+<script src="js/libraries/jquery-3.1.1.min.js"></script>
+<script src="js/libraries/jquery-ui.min.js"></script>
+<script src="js/libraries/bootstrap.min.js"></script>
+<script src="js/libraries/bootstrap-multiselect.js"></script>
+<script src="js/libraries/tooltipster.bundle.min.js"></script>
+
+<script src="js/functions.js"></script>
+<script src="js/events.js"></script>
+</body>
+</html>
--- includes/main_nav.php
+++ includes/main_nav.php
@@ -10,7 +10,7 @@
             <li class="nav-item dropdown <?php if(($current_page == 'philosophy') || ($current_page == 'press_list') || ($current_page == 'team')) {echo 'active';} ?>">
                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">L’agence</a>
                 <div class="dropdown-menu" aria-labelledby="Preview">
-                    <a class="dropdown-item <?php if($current_page == 'philosophy') {echo 'active';} ?>" href="#">Philisophie & competence</a>
+                    <a class="dropdown-item <?php if($current_page == 'agency') {echo 'active';} ?>" href="/agency.php">Philisophie & competence</a>
                     <a class="dropdown-item <?php if($current_page == 'press_list') {echo 'active';} ?>" href="/press_list.php">Revue de presse</a>
                     <a class="dropdown-item <?php if($current_page == 'team') {echo 'active';} ?>" href="/team.php">L’equipe</a>
                 </div>
